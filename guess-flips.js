de5c55f6f738b99468305be9f7edd5c41ea65c96
Jenna Levy <jennaslevy@gmail.com>
2022-04-16 17:16:40 -0400

    pls

--- guess-flips.js
+++ guess-flips.js
@@ -1,12 +1,13 @@
 import { flipACoin } from "./modules/coin.mjs";
 import minimist from "minimist";
 
-const args = minimist(process.argv)
+const args = minimist(process.argv.slice(2))
 args["call"]
 const call = args.call
 
 
 if (call == null || (call != "heads" && call != "tails") ){
+    console.log("Error: wrong or no input")
     console.log('Usage: node guess-flip --call=[heads||tails]')
 } else {
     console.log(flipACoin(call))
